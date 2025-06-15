075bb9b785b731095b1cc2fba90848e403f535d9
Akash <hafejmohammadakash@gmail.com>
2025-06-15 12:45:16 +0600

    dot rnc dot local name ekta file baniye tar moddhe token banalam

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCkOIsUpN7Ekh0RBs1jJroY1MRUHZhH8do",
-  authDomain: "job-protal-project.firebaseapp.com",
-  projectId: "job-protal-project",
-  storageBucket: "job-protal-project.firebasestorage.app",
-  messagingSenderId: "700621447237",
-  appId: "1:700621447237:web:9b1197ee1f6865b5a2032c"
+  apiKey: VITE_apiKey ,
+  authDomain: VITE_authDomain ,
+  projectId: VITE_projectId ,
+  storageBucket: VITE_storageBucket ,
+  messagingSenderId: VITE_messagingSenderId ,
+  appId: VITE_appId ,
 };
 
 // Initialize Firebase
