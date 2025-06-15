1380c5e8c2851bfa8b798bccf75535cf2f8f4f58
Akash <hafejmohammadakash@gmail.com>
2025-06-15 12:26:01 +0600

    register page banailam lottie file animation add korlam

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayoutes from '../MainLayouts.jsx/MainLayoutes';
 import Home from '../pages/Home';
+import Register from '../pages/Register';
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />
+            },
+            {
+                path: "/register",
+                element: <Register />
             },
         ]
     }
