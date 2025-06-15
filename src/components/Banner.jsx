b1877ca42945b053ace026cdcc7ab75c8a3b87cf
Akash <hafejmohammadakash@gmail.com>
2025-06-15 20:02:19 +0600

    motion diye animated korlam

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { motion } from "motion/react";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+        {/* images section */}
+        <div className="flex-1">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+        </div>
+        <div className="flex-1">
+          <motion.h1
+            animate={{ x: 50, color: ["red", "green"] }}
+            transition={{ duration: 2, repeat: Infinity }}
+            className="text-5xl font-bold"
+          >
+            Box Office News!
+          </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
