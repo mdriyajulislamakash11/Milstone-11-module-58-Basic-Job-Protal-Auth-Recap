72c4f4c210e03778cd96084534c2a5210c24c60b
Akash <hafejmohammadakash@gmail.com>
2025-06-15 20:19:51 +0600

    banner er jonno 3 ta image anam

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,48 @@
 import React from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        {/* images section */}
+        {/* Image Section */}
         <div className="flex-1">
           <img
             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
             className="max-w-sm rounded-lg shadow-2xl"
           />
         </div>
+
+        {/* Text Section */}
         <div className="flex-1">
           <motion.h1
-            animate={{ x: 50, color: ["red", "green"] }}
-            transition={{ duration: 2, repeat: Infinity }}
+            animate={{
+              x: [0, 50, 0],
+              color: ["#ff0000", "#00ff00", "#0000ff"],
+            }}
+            transition={{
+              duration: 3,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "easeInOut",
+            }}
             className="text-5xl font-bold"
           >
-            Box Office News!
+            Box Office{" "}
+            <motion.span
+              animate={{
+                color: ["#ff5733", "#f9b705", "#05aff9", "#ff5733"],
+              }}
+              transition={{
+                duration: 4,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut",
+              }}
+            >
+              Jobs
+            </motion.span>{" "}
+            News!
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
