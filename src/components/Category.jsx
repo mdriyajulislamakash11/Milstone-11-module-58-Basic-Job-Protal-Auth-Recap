fa18c14383a573b6bcd8ba33b1ac1745c90739ac
Akash <hafejmohammadakash@gmail.com>
2025-06-16 10:08:38 +0600

    category section start

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div>
+            <h2>Browse by category</h2>
+            <p>Find the job that’s perfect for you. about 800+ new jobs everyday</p>
+
+            {/* Categori Card */}
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Category;
