79bdfbfe2e4d1e916ebe2778e7947ecb18692a2c
Akash <hafejmohammadakash@gmail.com>
2025-06-16 20:40:12 +0600

    thik korlm

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -3,13 +3,13 @@ import { useLoaderData } from 'react-router-dom';
 
 const JobDetails = () => {
 
-    const {title, company, deadline} = useLoaderData()
+    const {title, company, applicationDeadline} = useLoaderData()
 
     return (
         <div>
             <h2 className='m'>Job Details For: {title}</h2>
             <p>Apply for: {company}</p>
-            <p>Deadline: {deadline}</p>
+            <p>Deadline: {applicationDeadline}</p>
             <button className='btn btn-primary'>Apply Now</button>
         </div>
     );
