6f35dfc27ac1599f2001de97ef0149e3a36d494e
Akash <hafejmohammadakash@gmail.com>
2025-06-17 16:57:25 +0600

    onk kosto kore application count ta korlam new sikhlam

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Auth/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const MyPostedJobs = () => {
     const [jobs, setJobs] = useState([])
@@ -16,8 +17,38 @@ const MyPostedJobs = () => {
 
     
     return (
-        <div>
-            posted jobs {jobs.length}
+         <div>
+            <h2 className='text-3xl'>My Posted Jobs: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Job Title</th>
+                            <th>Deadline</th>
+                            <th>Application Count</th>
+                            <th>Applications</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <tr key={job._id}>
+                                <th>{index + 1}</th>
+                                <td>{job.title}</td>
+                                <td>{job.applicationDeadline}</td>
+                                <td>{job.applicationCount}</td>
+                                <td>
+                                    <Link to={`/viewApplications/${job._id}`}>
+                                        <button className='btn btn-link'>View Applications</button>
+                                    </Link>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
