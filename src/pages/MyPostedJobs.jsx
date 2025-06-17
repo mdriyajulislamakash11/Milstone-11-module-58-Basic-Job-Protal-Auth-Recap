6a377f6266688874a2d50f868705c3e41161e1d0
Akash <hafejmohammadakash@gmail.com>
2025-06-17 14:19:29 +0600

    my posted job compote data dekhate parlam

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../Auth/AuthProvider';
 
 const MyPostedJobs = () => {
+    const [jobs, setJobs] = useState([])
+    const {user} = useContext(AuthContext)
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/jobs?email=${user.email}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setJobs(data)
+        })
+    }, [])
+
+    
     return (
         <div>
-            posted jobs
+            posted jobs {jobs.length}
         </div>
     );
 };
