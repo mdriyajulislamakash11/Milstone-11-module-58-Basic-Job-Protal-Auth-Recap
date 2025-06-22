116398c894093da891b46988e263ad92be8d228f
Akash <hafejmohammadakash@gmail.com>
2025-06-22 11:06:54 +0600

    ami jwt ke observer er moddhe axios diye set kolam

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -52,6 +53,14 @@ const AuthProvider = ({ children }) => {
       (auth,
       (currentUser) => {
         setUser(currentUser);
+
+        console.log('currentUser: ', currentUser.email)
+        const user = {email: currentUser?.email}
+        axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
+        .then(res => {
+          console.log(res.data)
+        })
+
         setLoading(false);
       });
 
