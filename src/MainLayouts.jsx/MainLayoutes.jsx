a8ecf3015048a51fe4c29919af972f788f6d9ae8
Akash <hafejmohammadakash@gmail.com>
2025-06-23 13:27:25 +0600

    footer add

--- src/MainLayouts.jsx/MainLayoutes.jsx
+++ src/MainLayouts.jsx/MainLayoutes.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayoutes = () => {
   return (
     <div>
-
-
-        <div className='container mx-auto'>
-            <Navbar />
-            <Outlet />
+      <div className="container mx-auto">
+        <Navbar />
+        <div className="min-h-[calc(100vh-317px)]">
+          <Outlet />
         </div>
 
-
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayoutes
+export default MainLayoutes;
