57254a8a96ee8710d79d8e0dcc294c621557b958
Akash <hafejmohammadakash@gmail.com>
2025-06-23 10:27:34 +0600

    hook full banano ses

--- src/hook/UseAxiosSecure.jsx
+++ src/hook/UseAxiosSecure.jsx
@@ -1,32 +1,38 @@
 import axios from "axios";
-import { useEffect } from "react";
-
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Auth/AuthProvider";
 
 const axiosIntance = axios.create({
-    baseURL: 'http://http://localhost:5000',
-    withCredentials: true
+  baseURL: "http://http://localhost:5000",
+  withCredentials: true,
 });
 
-
-
 const UseAxiosSecure = () => {
+  const { logOut } = useContext(AuthContext);
 
-    useEffect(() => {
-        axiosIntance.interceptors.response.use(response => {
-            return response;
-        }, error => {
-            console.log("error interceptors: ", error)
+  useEffect(() => {
+    axiosIntance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error interceptors: ", error);
 
-            if(error.status === 401 || error.status === 403){
-                console.log('need to logout user')
-                
-            }
+        if (error.status === 401 || error.status === 403) {
+          console.log("need to logout user");
+          logOut()
+          .then(() => {
+            console.log("logOut")
+          })
+          .catch(error => console.log(error))
+        }
 
-            return Promise.reject(error)
-        })
-    }, [])
+        return Promise.reject(error);
+      }
+    );
+  }, []);
 
-    return axiosIntance;
+  return axiosIntance;
 };
 
 export default UseAxiosSecure;
